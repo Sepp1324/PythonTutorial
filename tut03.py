fe5794b146c9dfdde4436be72b882901bd9fda7a
Sebastian Stögerer <SkyCrash13241234>
2020-05-13 17:45:01 +0200

    Datasets (Tuptle, Dict, Set, List)

---
+++ tut03.py
@@ -0,0 +1,18 @@
+# Leere Liste
+empty_list = ['Hallo', 'du', 'Schnecke']
+# empty_list = list()
+
+# Leeres Tuple
+empty_tuple = ('Hallo', 'du', 'Schnecke')
+# empty_tuple = tuple()
+
+# Leeres Set
+empty_set = {'Hallo', 'du', 'Schnecke'} # So ist es ein Dictionary
+# empty_set = set() # So ist es ein Srt
+
+list_str = ' '.join(empty_list)
+
+print(list_str)
+print(empty_list)
+print(empty_tuple)
+print(empty_set)
